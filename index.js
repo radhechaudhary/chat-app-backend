c495930d651b1a118f1ce74f6a488467b7052d24
radhechaudhary <radhechaudhary6398@gmai.com>
2025-02-04 23:56:00 +0530

    aa

--- index.js
+++ index.js
@@ -70,8 +70,7 @@ io.on('connection',(socket)=>{  // socket connection
     }
       socket.on("disconnect", (reason) => { // listner when socket disconnects
         const userId=Object.keys(users).find(key => users[key] === socket.id);
-        if (userId) {
-            delete users[userId];  
+        if (userId && (reason==='ping timeout' || reason==='transport close' || reason==='transport error')) {
             delete active_users[userId];  
             delete users_online[userId];  
             console.log(`User ${userId} removed from active users`);
